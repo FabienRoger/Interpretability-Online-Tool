4d055a6f74790e2188cb1cb69a9eb42acfb68b23
Fabien Roger <efab.roger@gmail.com>
2022-02-22 17:05:16 +0100

    Add fetch and basic graph

--- main.js
+++ main.js
@@ -1,1 +1,95 @@
-console.log("hello")
+const API_URL =
+  "https://a38go46a21.execute-api.us-east-1.amazonaws.com/default/ServerlessHuggingFaceStack-functionF19B1A04-HdK1WOXBtxwl";
+
+const submitButton = document.getElementById("submit");
+const textbox = document.getElementById("textbox");
+const inputTooShortAlert = document.getElementById("input-too-short");
+let resultChart = null;
+
+const process_table = (table) => {
+  const labels = table.map((item) => item[0]);
+  const values = table.map((item) => item[1]);
+  return {
+    labels,
+    values,
+  };
+};
+
+const get_chart_data = (labels, values) => {
+  return {
+    labels: labels,
+    datasets: [
+      {
+        label: "Fraction of the time neurons are activated",
+        backgroundColor: "rgb(50, 50, 200)",
+        borderColor: "rgb(25, 25, 100)",
+        data: values,
+      },
+    ],
+  };
+};
+
+const get_chart_config = (data) => {
+  return {
+    type: "bar",
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+};
+
+const draw_new_chart = (labels, values) => {
+  const data = get_chart_data(labels, values);
+  const config = get_chart_config(data);
+  resultChart = new Chart(document.getElementById("result-chart"), config);
+};
+
+const draw_chart = (table) => {
+  const { labels, values } = process_table(table);
+  if (resultChart) {
+    resultChart.config.data.labels = labels;
+    resultChart.config.data.datasets[0].data = values;
+    resultChart.update();
+  } else {
+    draw_new_chart(labels, values);
+  }
+};
+
+const fetch_API = (text) => {
+  fetch(API_URL, {
+    method: "POST",
+    body: "some text",
+    // mode: "cors",
+    // cache: "default",
+  })
+    .then((data) => data.blob())
+    .then((data) => data.text())
+    .then((data) => {
+      table = JSON.parse(data);
+      draw_chart(table);
+      console.log("data received!");
+      submitButton.disabled = false;
+    });
+};
+
+submitButton.onclick = () => {
+  if (textbox.value.length == 0) {
+    inputTooShortAlert.hidden = false;
+    return;
+  }
+
+  submitButton.disabled = true;
+  fetch_API();
+};
+
+textbox.oninput = () => {
+  console.log(textbox.value.length);
+  if (textbox.value.length > 0) {
+    inputTooShortAlert.hidden = true;
+  }
+};
